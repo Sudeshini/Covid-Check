6cac91f089de1a4775edeb3c1949ea2fe08e1b77
Sudeshini Thenuwara <58098705+Sudeshini@users.noreply.github.com>
2020-10-13 11:41:23 +0530

    covid check result

---
+++ covid_check.js
@@ -0,0 +1,22 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+rl.question('What is your city?', function(answer){
+    if (answer=='colombo'){
+        console.log("low risk area" + answer);
+    }
+
+    else if (answer=="galle"){
+        console.log("high risk area" + answer);
+    }
+    else{
+        console.log("incorrect");
+    }   
+    
+    rl.close();
+    
+})
